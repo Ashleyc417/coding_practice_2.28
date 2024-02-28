fb19e4810400bf1ff989ad652fba88005fd3b0a0
Ashley Chan <ashleyc417@csu.fullerton.edu>
2024-02-28 12:25:43 -0800

    Set up

---
+++ geometry_calculator.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath> // Include cmath for mathematical functions
+#include "geometry_calculator.h"
+
+using namespace std;
+
+double calculateRectangleArea(double length, double width) {
+    // TODO: Add an if statement to check if length and width are greater than zero
+    // If both are greater than zero, calculate and return the area
+    // Otherwise, print an error message and return -1
+}
+
+double calculateCircleCircumference(double radius) {
+    // TODO: Add an if statement to check if radius is greater than zero
+    // If it's greater than zero, calculate and return the circumference
+    // Otherwise, print an error message and return -1
+}
+
+double calculateTrianglePerimeter(double side1, double side2, double side3) {
+    // TODO: Add an if statement to check if all sides are greater than zero
+    // and if the sum of any two sides is greater than the third side
+    // If conditions are met, calculate and return the perimeter
+    // Otherwise, print an error message and return -1
+}
+
+void calculateQuotientAndRemainder(int dividend, int divisor) {
+    // TODO: Add an if statement to check if divisor is not zero
+    // If divisor is not zero, calculate quotient and remainder and print them
+    // Otherwise, print an error message
+}
+
+int main() {
+    // Test the functions here
+    // You can prompt the user for input and call the functions accordingly
+    return 0;
+}
---
+++ geometry_calculator.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+double calculateRectangleArea(double length, double width);
+
+double calculateCircleCircumference(double radius);
+
+double calculateTrianglePerimeter(double side1, double side2, double side3);
+
+void calculateQuotientAndRemainder(int dividend, int divisor);
