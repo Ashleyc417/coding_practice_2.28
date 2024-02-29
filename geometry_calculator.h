6b5129bc70c2ba23fd0eaa5fbb08a7bdfd3abc82
Ashley Chan <ashleyc417@csu.fullerton.edu>
2024-02-28 16:14:47 -0800

    update header file with ifndef, def, and endif macros

--- geometry_calculator.h
+++ geometry_calculator.h
@@ -1,9 +1,12 @@
+#ifndef GEOMETRY_CALCULATOR_H
+#define GEOMETRY_CALCULATOR_H
+
 /**
  * This is a header file for geometry_calculator.cpp
  * A header file is used to quickly reference functions/code you'll be
  * implementing in your .cpp or .cc file.
-*/ 
- 
+ */
+
 // This function computes the area of a rectangles.
 // Formula = length x width
 double calculateRectangleArea(double length, double width);
@@ -18,3 +21,5 @@ double calculateTrianglePerimeter(double side1, double side2, double side3);
 
 // This function calculates the quotient and remainder when using division.
 void calculateQuotientAndRemainder(int dividend, int divisor);
+
+#endif // GEOMETRY_CALCULATOR_H
